93d8e6afe42bcec9568080788e29b1b565cb7e96
ParadisdAbbadon <tmont091@gmail.com>
2026-02-19 00:03:51 -0800

    add queries

--- sql/queries/test_queries.sql
+++ sql/queries/test_queries.sql
@@ -1,3 +1,4 @@
+-- Query to select all motorcycles that are made by Yamaha and are blue in color.
 SELECT
     *
 FROM
@@ -5,3 +6,17 @@ FROM
 WHERE
     Make = 'Yamaha'
     AND Color = 'Blue';
+
+/*
+Query to select all motorcycles that are between a price greater than less than or equal to $7000
+and have engine sizes between 300cc and 700cc, ordered by price in ascending order.
+*/
+SELECT
+    *
+FROM
+    motorcycles
+WHERE
+    CCs BETWEEN 300 AND 700
+    AND Price <= 7000
+ORDER BY
+    Price ASC;
